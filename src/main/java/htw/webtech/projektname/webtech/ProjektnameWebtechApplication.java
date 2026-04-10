709cd85862034bb6658101fd54a48b53962c0546
LicaninAntonio <licaninantonio@gmail.com>
2026-04-10 08:26:55 +0200

    erster commit

---
+++ src/main/java/htw/webtech/projektname/webtech/ProjektnameWebtechApplication.java
@@ -0,0 +1,13 @@
+package htw.webtech.projektname.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektnameWebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektnameWebtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/webtech/projektname/webtech/ProjektnameWebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.projektname.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektnameWebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
